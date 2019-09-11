f3eb1544f6f5e28398e347c3cb90bfae810b853c
Arjun Nair <nair.arjun47@yahoo.com>
2019-09-11 20:38:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,12 @@ def display_board(board)
   puts "#{board} was chosen"
 end
 
+def input_to_index(input)
+  output = input.to_i
+  output -= 1
+  return output
+end
+
 def valid_move?(board, index)
   if index.between?(0,8) && (board == "X" || board == "O")
     return true 
